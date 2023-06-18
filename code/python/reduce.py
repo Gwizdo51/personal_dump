293efa0d01964aaf19dd5bb2e8bdbcc7e1e7589c
Gwizdo51 <arthur.clement.51@gmail.com>
2023-06-18 22:05:27 +0200

    reduce.py reorganised

--- code/python/reduce.py
+++ code/python/reduce.py
@@ -1,5 +1,6 @@
-# sum(), any(), all(), max(), min(), and len()
 from functools import reduce
+
+# sum(), any(), all(), max(), min(), and len()
 
 _len = lambda it: reduce(
     lambda value, element: value + 1,
@@ -34,11 +35,12 @@ _min = lambda it: reduce(
     it
 )
 
-print(_len(["a", "b", "c"]))
-print(_sum([3,1,2]))
-print(_any([False, False]))
-print(_any([False, True]))
-print(_all([True, True]))
-print(_all([False, True]))
-print(_max([1,5,-2]))
-print(_min([1,5,-2]))
+if __name__ == "__main__":
+    print(_len(["a", "b", "c"]))
+    print(_sum([3,1,2]))
+    print(_any([False, False]))
+    print(_any([False, True]))
+    print(_all([True, True]))
+    print(_all([False, True]))
+    print(_max([1,5,-2]))
+    print(_min([1,5,-2]))
