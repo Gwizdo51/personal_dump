f2444dcbabe1855b9b59cf9433a62b843a4b996f
Gwizdo51 <arthur.clement.51@gmail.com>
2023-07-01 05:41:41 +0200

    reorganised Confirm.ps1

--- code/powershell/Confirm.ps1
+++ code/powershell/Confirm.ps1
@@ -1,29 +1,3 @@
-# $title = 'Confirm'
-# $question = "Are you sure you want to perform this action?`nPerforming the operation `"Remove File`" on target `"D:\code\personal_dump\code\powershell\lol.txt`"."
-# # $choices = '&Yes', '&No'
-# $choice_yes = New-Object -TypeName Management.Automation.Host.ChoiceDescription '&Yes', 'Continue with only the next step of the operation.'
-# $choice_yesall = New-Object -TypeName Management.Automation.Host.ChoiceDescription 'Yes to &All', 'Continue with all the steps of the operation.'
-# $choice_no = New-Object -TypeName Management.Automation.Host.ChoiceDescription '&No', 'Skip this operation and proceed with the next operation.'
-# $choice_noall = New-Object -TypeName Management.Automation.Host.ChoiceDescription 'No to A&ll', 'Skip this operation and all subsequent operations.'
-# $choice_suspend = New-Object -TypeName Management.Automation.Host.ChoiceDescription '&Suspend', 'Pause the current pipeline and return to the command prompt. Type "exit" to resume the pipeline.'
-# $choices = $choice_yes, $choice_yesall, $choice_no, $choice_noall, $choice_suspend
-# $decision = $Host.UI.PromptForChoice($title, $question, $choices, 0)
-# $decision
-
-function default_confirmation_prompt {
-    param([string]$question_new_line)
-    $title = 'Confirm'
-    $question = 'Are you sure you want to perform this action?'
-    if ($question_new_line) {$question = $question + "`n" + $question_new_line}
-    $choice_yes = New-Object -TypeName Management.Automation.Host.ChoiceDescription '&Yes', 'Continue with only the next step of the operation.'
-    $choice_yesall = New-Object -TypeName Management.Automation.Host.ChoiceDescription 'Yes to &All', 'Continue with all the steps of the operation.'
-    $choice_no = New-Object -TypeName Management.Automation.Host.ChoiceDescription '&No', 'Skip this operation and proceed with the next operation.'
-    $choice_noall = New-Object -TypeName Management.Automation.Host.ChoiceDescription 'No to A&ll', 'Skip this operation and all subsequent operations.'
-    $choice_suspend = New-Object -TypeName Management.Automation.Host.ChoiceDescription '&Suspend', 'Pause the current pipeline and return to the command prompt. Type "exit" to resume the pipeline.'
-    $choices = $choice_yes, $choice_yesall, $choice_no, $choice_noall, $choice_suspend
-    $Host.UI.PromptForChoice($title, $question, $choices, 0)
-}
-
 function confirmation_prompt {
     param(
         [string] $Question = 'Are you sure you want to perform this action?',
@@ -33,4 +7,18 @@ function confirmation_prompt {
     $title = 'Confirm'
     $choices = $ChoicesTable.Keys | sort | % {New-Object -TypeName Management.Automation.Host.ChoiceDescription $ChoicesTable[$_][0], $ChoicesTable[$_][1]}
     $Host.UI.PromptForChoice($title, $question, $choices, $DefaultChoice)
+}
+
+function default_confirmation_prompt {
+    param([string]$QuestionNewLine)
+    $Question = 'Are you sure you want to perform this action?'
+    if ($QuestionNewLine) {$Question = $Question + "`n" + $QuestionNewLine}
+    $choices_table = @{
+        0 = '&Yes', 'Continue with only the next step of the operation.';
+        1 = 'Yes to &All', 'Continue with all the steps of the operation.';
+        2 = '&No', 'Skip this operation and proceed with the next operation.';
+        3 = 'No to A&ll', 'Skip this operation and all subsequent operations.';
+        4 = '&Suspend', 'Pause the current pipeline and return to the command prompt. Type "exit" to resume the pipeline.'
+    }
+    confirmation_prompt -Question $Question -ChoicesTable $choices_table
 }
