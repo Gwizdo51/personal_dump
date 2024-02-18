4efb333c742977cde85c022b8bd3e5a0d2731651
Gwizdo51 <arthur.clement.51@gmail.com>
2024-02-18 22:12:41 +0100

    added a Recycle-Item cmdlet

--- code/powershell/utils/Recycle.ps1
+++ code/powershell/utils/Recycle.ps1
@@ -3,7 +3,8 @@
 #     if ($f -or $force) {Remove-Item $args -r -force}
 #     else {Remove-Item $args -r}
 # }
-function Recycle { # move items to trash on "rm" calls
+<#
+function Recycle-Item { # move items to trash on "rm" calls
     # needs to check if recycle is used on an item from the FileSystem drive
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
     Param(
@@ -13,7 +14,6 @@ function Recycle { # move items to trash on "rm" calls
         [switch] $Yes
     )
     # get the absolute path of the item + check if item exists
-    # try - catch here
     $path_is_valid = Test-Path $Path
     if (-not $path_is_valid) {
         # Write-Error "$Path does not exist"; return $null
@@ -49,7 +49,7 @@ function Recycle { # move items to trash on "rm" calls
     # $shell_item | Get-Member
     # return
     # request confirmation if the item is a folder (doesn't matter if it's a link) -> maybe not ?
-    if ($shell_item.IsFolder -and !($shell_item.IsLink) -and !$yes) {
+    if ($shell_item.IsFolder -and !($shell_item.IsLink) -and !$Yes) {
         $original_confirmation_message = "Confirm recycling folder '$absolute_path' ([Y]es/No)"
         $confirmation_message = $original_confirmation_message
         # while (-not $(Test-Path 'Variable:\confirmed')) {
@@ -87,9 +87,78 @@ function Recycle { # move items to trash on "rm" calls
     if ($path_exists) {Write-Error 'Some items were not removed'}
     else {Write-Host "Moved '$($color_characters_dict.col_Red)$($absolute_path)$($color_characters_dict.col_def)' to the recycle bin"}
 }
-New-Item -Path Alias:rc -Value Recycle -Force > $null
+#>
+
+function Recycle-Item {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    Param(
+        [Parameter(Mandatory, ValueFromPipeline)] # check pipeline
+        [SupportsWildcards()] # check wildcards (https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_wildcards?view=powershell-7.3)
+        [string[]] $Path # should be able to take a list of paths
+    )
+    begin {
+        $all_filesystem_roots = (Get-PSDrive | where {$_.provider.name -eq 'FileSystem'}).root
+        $shell = New-Object -ComObject 'Shell.Application'
+    }
+    process {
+        foreach ($path_item in $Path) {
+            # Write-Host "recycling '${path_item}'"
+            if (!(Test-Path $path_item)) {
+                $ErrorRecord = [System.Management.Automation.ErrorRecord]::new(
+                    [System.IO.FileNotFoundException] "Cannot find path '${path_item}' because it does not exist",
+                    'ItemNotFound',
+                    [System.Management.Automation.ErrorCategory]::ObjectNotFound,
+                    $path_item
+                )
+                $PSCmdlet.WriteError($ErrorRecord)
+                continue
+            }
+            # Write-Host "recycling '${path_item}'"
+            # make a pipeline to delete all items pointed by $path_item
+            $path_item | Get-Item -Force | ? {
+                # check if the item is a FileSystem item
+                if ($_.PSProvider.Name -eq 'FileSystem') {$True}
+                else {
+                    $ErrorRecord = [System.Management.Automation.ErrorRecord]::new(
+                        [System.InvalidOperationException] 'Can only recycle files, folders and links',
+                        'InvalidProvider',
+                        [System.Management.Automation.ErrorCategory]::PermissionDenied,
+                        $_
+                    )
+                    $PSCmdlet.WriteError($ErrorRecord)
+                    $False
+                }
+            } | ? {
+                # check if the item is the root of a drive
+                if ($_.FullName -in $all_filesystem_roots) {
+                    $ErrorRecord = [System.Management.Automation.ErrorRecord]::new(
+                        [System.ArgumentException] 'Cannot recycle a FileSystem root',
+                        'InvalidItem',
+                        [System.Management.Automation.ErrorCategory]::InvalidArgument,
+                        $_
+                    )
+                    $PSCmdlet.WriteError($ErrorRecord)
+                    $False
+                }
+                else {$True}
+            } | ? {$PSCmdlet.ShouldProcess($_, 'Recycle')} | % {
+                # $_
+                $parent_dir_path = Split-Path $_.FullName -Parent
+                # $item_name = Split-Path $_.FullName -Leaf
+                # $parent_dir_path, $item_name
+                # $shell = New-Object -ComObject 'Shell.Application'
+                $shell_dir = $shell.Namespace($parent_dir_path)
+                $shell_item = $shell_dir.ParseName($_.Name)
+                # $shell_item
+                $shell_item.InvokeVerb('Delete')
+            }
+        }
+    }
+}
+New-Item -Path Alias:ri -Value Recycle-Item -Force > $null
 
 function Open-RecyleBin {start shell:RecycleBinFolder}
+New-Item -Path Alias:orb -Value Open-RecyleBin -Force > $null
 
 # list all recycled items: (New-Object -ComObject shell.application).Namespace(10).Items()
 # access specific recycled item:
